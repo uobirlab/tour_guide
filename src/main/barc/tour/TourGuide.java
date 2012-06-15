9b6ee1fc7aca725040ca7f72cb36fbf9887183b1
Jeremiah Via <jeremiah.via@gmail.com>
2012-06-15 14:41:59 +0100

    Upgrade to new rosjava.

--- src/main/barc/tour/TourGuide.java
+++ src/main/barc/tour/TourGuide.java
@@ -1,30 +1,21 @@
 package barc.tour;
 
-import org.ros.message.geometry_msgs.Twist;
-import org.ros.node.DefaultNodeFactory;
-import org.ros.node.Node;
-import org.ros.node.NodeConfiguration;
-import org.ros.node.NodeMain;
-import org.ros.node.topic.Publisher;
+import org.ros.namespace.GraphName;
+import org.ros.node.AbstractNodeMain;
+import org.ros.node.ConnectedNode;
 
 
 /**
  * @author Jeremiah Via <jxv911@cs.bham.ac.uk>
  */
-public class TourGuide implements NodeMain {
-
-    private Node node;
-    private Publisher<Twist> pub;
-
+public class TourGuide extends AbstractNodeMain {
     @Override
-    public void main(NodeConfiguration nodeConfiguration) throws Exception {
-        node = new DefaultNodeFactory().newNode("tour_guide", nodeConfiguration);
-        pub = node.newPublisher("cmd_vel", "geometry_msgs/Twist");
+    public GraphName getDefaultNodeName() {
+        return null;  //To change body of implemented methods use File | Settings | File Templates.
     }
 
     @Override
-    public void shutdown() {
-        node.shutdown();
-        node = null;
+    public void onStart(ConnectedNode connectedNode) {
+        super.onStart(connectedNode);    //To change body of overridden methods use File | Settings | File Templates.
     }
 }
